a80c937cd0f6fa215fe9b4ed1cedb475a9a19a84
Tristan Sloughter <t@crashfast.com>
2019-02-25 14:34:23 -0700

    upgrade support to opencensus 0.9

--- test/opencensus/plug/trace_test.exs
+++ test/opencensus/plug/trace_test.exs
@@ -4,13 +4,6 @@ defmodule Opencensus.Plug.TraceTest do
 
   alias Opencensus.Plug.Trace, as: Subject
 
-  @trace_version "00"
-  @trace_id "00000000000000000000000000000001"
-  @span_id "0000000000000001"
-  @trace_options "01"
-
-  @encoded Enum.join([@trace_version, @trace_id, @span_id, @trace_options], "-")
-
   doctest Subject
 
   setup do
@@ -45,14 +38,13 @@ defmodule Opencensus.Plug.TraceTest do
     test "after response the parent span continues", %{conn: conn} do
       ctx = :oc_trace.start_span("span", :undefined, %{})
 
-      header =
-        ctx
-        |> :oc_span_ctx_header.encode()
-        |> List.to_string()
+      headers =
+        for {k, v} <- :oc_propagation_http_tracecontext.to_headers(ctx) do
+          {k, List.to_string(v)}
+        end
 
       _ =
-        conn
-        |> put_req_header(:oc_span_ctx_header.field_name(), header)
+        List.foldl(headers, conn, fn {k, v}, acc -> Plug.Conn.put_req_header(acc, k, v) end)
         |> SamplePlug.call([])
         |> send_resp(200, "")
 
@@ -83,40 +75,9 @@ defmodule Opencensus.Plug.TraceTest do
     end
 
     test "sets response header", %{conn: conn, ctx: ctx} do
-      conn = Subject.put_ctx_resp_header(conn, "trace-header", ctx)
+      conn = Subject.put_ctx_resp_header(conn, ctx)
 
-      assert [_] = get_resp_header(conn, "trace-header")
-    end
-
-    test "response header is always lowercased", %{conn: conn, ctx: ctx} do
-      conn = Subject.put_ctx_resp_header(conn, "Trace-Header", ctx)
-
-      assert [_] = get_resp_header(conn, "trace-header")
-    end
-  end
-
-  describe "load_ctx/2" do
-    setup do: [conn: conn(:get, "/")]
-
-    test "sets current span context when header is present", %{conn: conn} do
-      conn =
-        conn
-        |> put_req_header("trace-header", @encoded)
-
-      assert :ok = Subject.load_ctx(conn, "trace-header")
-      assert :undefined != :ocp.current_span_ctx()
-    end
-
-    test "do not set context when header is empty", %{conn: conn} do
-      assert :ok = Subject.load_ctx(conn, "trace-header")
-      assert :undefined = :ocp.current_span_ctx()
-    end
-
-    test "do not set context when header is invalid", %{conn: conn} do
-      conn = put_req_header(conn, "trace-header", "foo-bar")
-
-      assert :ok = Subject.load_ctx(conn, "trace-header")
-      assert :undefined = :ocp.current_span_ctx()
+      assert [_] = get_resp_header(conn, "traceparent")
     end
   end
 end
